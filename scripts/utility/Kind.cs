7843696201ce5bbbc162a35d5ecb1c931101b317
Devon Albert <devon.drake.albert@outlook.com>
2025-08-11 15:27:27 -0700

    Add Rule and Kind Utility Classes

---
+++ scripts/utility/Kind.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace SquareVerse.Utility;
+
+public class Kind
+{
+    public Color Color;
+    public List<Rule> Rules { get; private set; }
+
+    public Kind(Color color)
+    {
+        Color = color;
+        Rules = [];
+    }
+
+    public void AddRule(Rule rule)
+    {
+        Rules.Add(rule);
+    }
+}
---
+++ scripts/utility/Rule.cs
@@ -0,0 +1,16 @@
+﻿namespace SquareVerse.Utility;
+
+public record struct Neighborhood(int TL, int Top, int TR, int Left, int Right, int BL, int Bottom, int BR);
+
+public struct Rule
+{
+    public Neighborhood Neighborhood;
+    
+    public int NewCenter;
+
+    public Rule(Neighborhood neighborhood, int newCenter)
+    {
+        Neighborhood = neighborhood;
+        NewCenter = newCenter;
+    }
+}
