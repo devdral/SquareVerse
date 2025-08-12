6679b1df293bc480b117460944d6c1d8ec18a11a
Devon Albert <devon.drake.albert@outlook.com>
2025-08-12 09:34:01 -0700

    Implement a basic top-panel UI with mode switcher

---
+++ scripts/ModeSwitch.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class ModeSwitch : VBoxContainer
+{
+	private Button EnterEdit;
+	private Button EnterView;
+
+	public override void _Ready()
+	{
+		EnterEdit = (Button)GetNode("EnterEdit");
+		EnterView = (Button)GetNode("EnterView");
+	}
+
+	public override void _Process(double delta)
+	{
+		EnterEdit.Disabled = UIManager.Instance.Mode;
+		EnterView.Disabled = !UIManager.Instance.Mode;
+	}
+
+	public void OnEnterEdit()
+	{
+		UIManager.Instance.Mode = UIManager.EDIT;
+		EnterEdit.Disabled = true;
+		EnterView.Disabled = false;
+	}
+	
+	public void OnEnterView()
+	{
+		UIManager.Instance.Mode = UIManager.VIEW;
+		EnterView.Disabled = true;
+		EnterEdit.Disabled = false;
+	}
+}
