26f0fd57d9a271a348fce9801c745d24b629b745
Devon Albert <devon.drake.albert@outlook.com>
2025-08-12 20:46:59 -0700

    Give AddKindPopup a Signal for when it creates the kind

--- scripts/AddKindPopup.cs
+++ scripts/AddKindPopup.cs
@@ -4,24 +4,34 @@ using SquareVerse.Utility;
 
 public partial class AddKindPopup : Window
 {
-    private ColorPickerButton colorPicker;
-    
-    public void OnAddKindPressed()
-    {
-        Show();
-    }
+	[Signal]
+	public delegate void KindCreatedEventHandler(int kindId);
+	
+	private ColorPickerButton colorPicker;
 
-    public void OnConfirmPressed()
-    {
-        Hide();
-        GridManager.Instance.Kinds.Add(new Kind(
-            colorPicker.Color
-            ));
-    }
+	public override void _Ready()
+	{
+		colorPicker = GetNode<ColorPickerButton>("FormItems/ColorPick/ColorPickerButton");
+	}
+	
+	public void OnAddKindPressed()
+	{
+		Show();
+	}
 
-    public void OnClose()
-    {
-        Hide();
-        colorPicker.Color = new Color("black");
-    }
+	public void OnConfirmPressed()
+	{
+		Hide();
+		var index = GridManager.Instance.Kinds.Count; 
+		GridManager.Instance.Kinds.Add(new Kind(
+			colorPicker.Color
+			));
+		EmitSignalKindCreated(index);
+	}
+
+	public void OnClose()
+	{
+		Hide();
+		colorPicker.Color = new Color("black");
+	}
 }
